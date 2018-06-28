14141cfbc044d78414c0d37fce3abdf5f09efebe
ande13 <avdemashov_10@yahoo.com>
2018-06-28 23:12:45 +0300

    service changes

--- src/main/java/com/revolut/dao/MoneyTransferRepository.java
+++ src/main/java/com/revolut/dao/MoneyTransferRepository.java
@@ -4,34 +4,25 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import org.hibernate.Session;
 
-import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
-import javax.persistence.TransactionRequiredException;
 
 @Singleton
 public class MoneyTransferRepository implements TransferRepository {
 
-    private EntityManager entityManager;
+    private Session session;
 
     @Inject
-    public MoneyTransferRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public void setSession(EntityManager entityManager) {
+        this.session = entityManager.unwrap(Session.class);
     }
 
     @Override
     public AccountEntity getEntityById(long id) {
-        return entityManager.find(AccountEntity.class, id);
+        return session.get(AccountEntity.class, id);
     }
 
     @Override
     public void save(AccountEntity accountEntity) {
-        Session unwrap = entityManager.unwrap(Session.class);
-        unwrap.getTransaction().begin();
-        try {
-            entityManager.persist(accountEntity);
-            unwrap.getTransaction().commit();
-        } catch (EntityExistsException | IllegalArgumentException| TransactionRequiredException ex) {
-            unwrap.getTransaction().rollback();
-        }
+        session.update(accountEntity);
     }
 }
--- src/main/java/com/revolut/services/MoneyTransferServiceImpl.java
+++ src/main/java/com/revolut/services/MoneyTransferServiceImpl.java
@@ -6,7 +6,11 @@ import com.revolut.controllers.dto.TransferResponse;
 import com.revolut.dao.AccountEntity;
 import com.revolut.dao.TransferRepository;
 import com.revolut.exceptions.TransferException;
+import org.hibernate.Session;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.TransactionRequiredException;
 import java.math.BigDecimal;
 
 @Singleton
@@ -14,6 +18,13 @@ public class MoneyTransferServiceImpl implements MoneyTransferService {
 
     @Inject
     private TransferRepository transferRepository;
+
+    private Session session;
+
+    @Inject
+    public void setSession(EntityManager entityManager) {
+        this.session = entityManager.unwrap(Session.class);
+    }
 
     @Override
     public TransferResponse transferMoney(Long fromAccountId, Long toAccountId, BigDecimal amount) {
@@ -56,14 +67,16 @@ public class MoneyTransferServiceImpl implements MoneyTransferService {
         BigDecimal toAccountBalance = to.getBalance().add(amount);
         to.setBalance(toAccountBalance);
 
-        save(from);
-        save(to);
+        try {
+            session.getTransaction().begin();
+            transferRepository.save(from);
+            transferRepository.save(to);
+            session.getTransaction().commit();
+        } catch (EntityExistsException | IllegalArgumentException| TransactionRequiredException ex) {
+            session.getTransaction().rollback();
+        }
 
         return new TransferResponse("The operation was successfully carried out!", fromAccountId, fromAccountBalance, toAccountId, toAccountBalance, amount);
-    }
-
-    private void save(AccountEntity accountEntity) {
-        transferRepository.save(accountEntity);
     }
 
 }
